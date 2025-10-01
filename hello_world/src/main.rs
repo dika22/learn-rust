00e29d52b5e958b50ee1561492e5621fa5941b00
Adhika pamungkas <adhikapamungkas22@gmail.com>
2025-10-01 13:50:54 +0700

    learn let and return

---
+++ hello_world/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("Hello, world!");
+
+    let x = 5;
+    println!("x is {}", x);
+
+    let x = x + 1;
+    println!("x is {}", x);
+
+    // mutable variables
+    let mut y = 5;
+    println!("y is {}", y);
+    y = y + 1;
+    println!("y is {}", y);
+}
---
+++ return_example/src/main.rs
@@ -0,0 +1,8 @@
+
+fn sum(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn main() {
+    println!("Hello, this sum is {}", sum(1, 2));
+}
